9efe8f94a01907a0f35b33e890644d7fd76389e7
HasimD <delibasahmet@gmail.com>
2019-01-20 22:51:43 +0300

    Back button on fridge page

--- app/src/main/java/com/koala/sloth/TabOrder/ActivityFridge.java
+++ app/src/main/java/com/koala/sloth/TabOrder/ActivityFridge.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.ListView;
 import com.koala.sloth.Providers.FridgeProvider;
 import com.koala.sloth.R;
+import android.view.MenuItem;
 
 public class ActivityFridge extends AppCompatActivity {
     private ListView listView;
@@ -20,12 +21,34 @@ public class ActivityFridge extends AppCompatActivity {
 
         load();
     }
+    public void onBackPressed() {
+        cancelFromCategory();
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
+        if (id == android.R.id.home) {
+            cancelFromCategory();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void cancelFromCategory() {
+            finish();
+    }
 
     private void load() {
         listView = findViewById(R.id.listView);
         listView.setAdapter(new FridgeCategory_Adapter(ActivityFridge.this, FridgeProvider.getFridgeCategories(this)));
 
+
+        actionBar = getSupportActionBar();
+        if (actionBar!=null) {
+            actionBar.setTitle("Fridge");
+
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
 
